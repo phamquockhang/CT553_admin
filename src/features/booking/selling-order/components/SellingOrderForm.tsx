b50470e39efdc9099a4601a21ee5050110628c84
Pham Quoc Khang <phamquockhang1108@gmail.com>
2025-03-08 02:50:06 +0700

    feat(selling order): refactor SellingOrderForm to integrate AddCustomerToOrderForm and remove unused components

--- src/features/booking/selling-order/components/SellingOrderForm.tsx
+++ src/features/booking/selling-order/components/SellingOrderForm.tsx
@@ -1,17 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import {
-  Button,
-  Card,
-  Col,
-  Collapse,
-  CollapseProps,
-  Form,
-  Input,
-  Select,
-  Space,
-  Switch,
-  Tooltip,
-} from "antd";
+import { Button, Form, Input, Select, Space, Tooltip } from "antd";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { IoIosAlert } from "react-icons/io";
@@ -24,11 +12,9 @@ import {
 } from "../../../../interfaces";
 import { sellingOrderService } from "../../../../services";
 import { translateOrderStatus } from "../../../../utils";
-import AddCustomer from "../../../auth/customers/AddCustomer";
-import AddAddress from "../../../auth/customers/components/AddAddress";
 import { useValidSellingOrderStatuses } from "../hooks/useValidSellingOrderStatuses";
+import AddCustomerToOrderForm from "./AddCustomerToOrderForm";
 import AddProductToOrderForm from "./AddProductToOrderForm";
-import FindCustomer from "./FindCustomer";
 import SellingOrderDetails from "./SellingOrderDetails";
 import SellingOrderStatusHistory from "./SellingOrderStatusHistory";
 
@@ -54,15 +40,11 @@ const SellingOrderForm: React.FC<SellingOrderFormProps> = ({
   const [districtId, setDistrictId] = useState<number>();
   const [wardCode, setWardCode] = useState<string>();
   const [description, setDescription] = useState<string>();
-
   const [formattedAddress, setFormattedAddress] = useState<string>();
 
   const address = choosenCustomer?.addresses.find(
     (address) => address.isDefault,
   );
-
-  const currentStatus = sellingOrderToUpdate?.orderStatus as OrderStatus;
-  const optionsOrderStatus = useValidSellingOrderStatuses(currentStatus);
 
   const { mutate: createSellingOrder } = useMutation({
     mutationFn: (newSellingOrder: Omit<ISellingOrder, "sellingOrderId">) => {
@@ -154,10 +136,6 @@ const SellingOrderForm: React.FC<SellingOrderFormProps> = ({
     }
   };
 
-  const handleCheck = (checked: boolean) => {
-    setHasCreateCustomer(checked);
-  };
-
   useEffect(() => {
     if (choosenCustomer) {
       form.setFieldsValue({
@@ -183,134 +161,10 @@ const SellingOrderForm: React.FC<SellingOrderFormProps> = ({
       : OrderStatus.COMPLETED,
   };
 
-  const items: CollapseProps["items"] = [
-    {
-      key: "createCustomer",
-      label: "Có lưu lại thông tin khách hàng không?",
-      extra: (
-        <Switch
-          checkedChildren="Có"
-          unCheckedChildren="Không"
-          defaultChecked={false}
-          value={hasCreateCustomer}
-          // disabled={viewOnly}
-          // checked={isModuleChecked(module)}
-          // onClick={(_, event) => event.stopPropagation()}
-          onChange={(checked) => handleCheck(checked)}
-        />
-      ),
-
-      children: (
-        <>
-          <Col key={`1`} span={24}>
-            <Card size="small">
-              <div className="mb-4 flex justify-between gap-8">
-                <FindCustomer
-                  viewMode={!hasCreateCustomer}
-                  setChoosenCustomer={setChoosenCustomer}
-                />
-
-                <AddCustomer />
-              </div>
-              <div className="flex gap-8">
-                <Form.Item
-                  className="flex-1"
-                  label="Tên khách hàng"
-                  name="customerName"
-                  rules={[
-                    {
-                      required: hasCreateCustomer,
-                      message: "Vui lòng nhập tên khách hàng",
-                    },
-                  ]}
-                >
-                  <Input
-                    disabled={!hasCreateCustomer}
-                    // readOnly={!hasCreateCustomer}
-                    placeholder="Nhập tên khách hàng"
-                  />
-                </Form.Item>
-
-                <Form.Item
-                  className="flex-1"
-                  label="Số điện thoại"
-                  name="phone"
-                  rules={[
-                    {
-                      required: hasCreateCustomer,
-                      message: "Vui lòng nhập số điện thoại",
-                    },
-                    {
-                      pattern: new RegExp("^(0|\\+84)\\d{9,10}$", "g"),
-                      message: "Số điện thoại không hợp lệ",
-                    },
-                  ]}
-                >
-                  <Input
-                    disabled={!hasCreateCustomer}
-                    // readOnly={!hasCreateCustomer}
-                    placeholder="Nhập số điện thoại"
-                  />
-                </Form.Item>
-              </div>
-
-              <div className="flex gap-8">
-                <Form.Item
-                  className="flex-1"
-                  label="Email"
-                  name="email"
-                  rules={[
-                    {
-                      required: hasCreateCustomer,
-                      message: "Vui lòng nhập email",
-                    },
-                    {
-                      type: "email",
-                      message: "Email không hợp lệ",
-                    },
-                  ]}
-                >
-                  <Input
-                    disabled={!hasCreateCustomer}
-                    // readOnly={!hasCreateCustomer}
-                    placeholder="Nhập email"
-                  />
-                </Form.Item>
-              </div>
-
-              <h1 className="mb-1 text-lg font-semibold">Địa chỉ giao hàng</h1>
-              <AddAddress
-                viewMode={!hasCreateCustomer}
-                form={form}
-                provinceId={provinceId}
-                setProvinceId={setProvinceId}
-                districtId={districtId}
-                setDistrictId={setDistrictId}
-                wardCode={wardCode}
-                setWardCode={setWardCode}
-                description={description}
-                setDescription={setDescription}
-                setFormattedAddress={setFormattedAddress}
-              />
-
-              <div className="flex gap-8">
-                <Form.Item className="flex-1" label="Ghi chú" name="note">
-                  <Input.TextArea
-                    disabled={!hasCreateCustomer}
-                    // readOnly={!hasCreateCustomer}
-                    rows={1}
-                    placeholder="Nhập ghi chú"
-                  />
-                </Form.Item>
-              </div>
-            </Card>
-          </Col>
-        </>
-      ),
-    },
-  ];
-
   // console.log("choosenCustomer", choosenCustomer);
+
+  const currentStatus = sellingOrderToUpdate?.orderStatus as OrderStatus;
+  const optionsOrderStatus = useValidSellingOrderStatuses(currentStatus);
 
   return (
     <Form
@@ -320,11 +174,25 @@ const SellingOrderForm: React.FC<SellingOrderFormProps> = ({
       onFinish={handleSubmit}
     >
       {!sellingOrderToUpdate && (
-        <>
-          <Collapse className="mb-6" items={items} />
+        <div className="mb-6 flex flex-col gap-4">
+          <AddCustomerToOrderForm
+            form={form}
+            hasCreateCustomer={hasCreateCustomer}
+            setHasCreateCustomer={setHasCreateCustomer}
+            setChoosenCustomer={setChoosenCustomer}
+            setFormattedAddress={setFormattedAddress}
+            provinceId={provinceId}
+            setProvinceId={setProvinceId}
+            districtId={districtId}
+            setDistrictId={setDistrictId}
+            wardCode={wardCode}
+            setWardCode={setWardCode}
+            description={description}
+            setDescription={setDescription}
+          />
 
           <AddProductToOrderForm />
-        </>
+        </div>
       )}
 
       {sellingOrderToUpdate && (
